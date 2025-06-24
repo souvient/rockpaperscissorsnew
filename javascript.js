852b22da2c049ec97a29fef794414e699ff5d182
Souvient <175044784+souvient@users.noreply.github.com>
2025-06-24 15:54:37 +0700

    Finished the fourth part of the project
    Wrote the code for playing one round of rock paper scissors

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     let randomOutcome = Math.floor(Math.random() * 3) + 1;
     if (randomOutcome === 1) {
@@ -12,11 +16,53 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     
-    let userInput = prompt("Rock, paper or scissors?")
+    let userInput = prompt("Rock, paper or scissors?");
 
     return userInput;
 
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        return "You lose! Paper beats rock!";
+        
+
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        return "You lose! Scissors beat paper" 
+
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        return "You lose! Rock beats scissors";
+
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        return "You win! Scissors beat paper";
+
+    
+
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        return "You win! Paper beats rock";
+
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        return "You win! Rock beats scissors";
+
+    } else if (humanChoice === computerChoice) {
+        return "Try again! You've chosen the same optin as the computer"
+    }
+
+    }
+
+    
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 
 
